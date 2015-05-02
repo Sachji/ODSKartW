905c34a6808ceb56f416b19495b61c199675ebab
Sachji <sachji@seznam.cz>
2015-05-02 13:36:08 +0200

    toString() fixed, added equals,hashcode

--- ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Movie.java
+++ ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Movie.java
@@ -1,8 +1,32 @@
 package cz.muni.fi.pb138.odskart.backend;
 
+import java.util.Objects;
+
 public class Movie {
 
     private String name;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Movie other = (Movie) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
 
     public Movie(String name) {
         if (name == null || name.equals("")) {
