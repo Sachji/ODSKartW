1c3daa4fac2b84d77b0df1b6a3a683a4a0e4f8d6
Sachji <sachji@seznam.cz>
2015-05-02 13:38:32 +0200

    toString() changed

--- ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Medium.java
+++ ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Medium.java
@@ -40,33 +40,12 @@ public class Medium {
         this.movies.add(movie);
     }
 
-    public Integer findMovie(String name) {
+    public Integer getMovie(String name) {
         if (name == null || name.equals("")) {
             throw new IllegalArgumentException("Movie name is null or empty string");
         }
-
         // if found return index otherwise -1
         return movies.indexOf(new Movie(name));
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 37 * hash + Objects.hashCode(this.movies);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof Medium)) {
-            return false;
-        }
-        // TODO
-
-        return true;
     }
 
     @Override
@@ -74,8 +53,9 @@ public class Medium {
 
         StringBuilder listString = new StringBuilder();
         for (Movie s : movies) {
-            listString.append(s.getName()).append(" ");
+            listString.append(s.getName()).append(",");
         }
+        listString.deleteCharAt(listString.length() - 1);
 
         return "Medium{" + "movies=" + listString + '}';
     }
