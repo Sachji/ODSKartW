e8b8e5eb96516d2861d7e65b2c2b631b2693a1f0
VojtaOndrej <Vojtech-Ondrej@seznam.cz>
2015-05-26 01:35:23 +0200

    MediumTest updated.

--- ODSKart/src/test/java/cz/muni/fi/pb138/odskart/backend/MediumTest.java
+++ ODSKart/src/test/java/cz/muni/fi/pb138/odskart/backend/MediumTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pb138.odskart.backend;
 
 import java.util.ArrayList;
@@ -36,34 +31,36 @@ public class MediumTest {
     }
 
     @Test
-    public void idIsLessEqualZeroTest() {
-        expected.expect(IllegalArgumentException.class);
+    public void idIsLessEqualZeroTest() {        
         Medium medium = new Medium(new Category(1, 1, "new"));
         Integer id = 0;
+        expected.expect(IllegalArgumentException.class);
         medium.setId(id);
     }
 
     @Test
     public void setCategoryNullTest() {
-        expected.expect(IllegalArgumentException.class);
         Category category = null;
         Medium medium = new Medium(new Category(1, 1, "new"));
+        expected.expect(IllegalArgumentException.class);
         medium.setCategory(category);
     }
 
     @Test
     public void getMovieNameNullTest() {
-        expected.expect(IllegalArgumentException.class);
+        
         String name = null;
         Medium medium = new Medium(new Category(1, 1, "new"));
+        expected.expect(IllegalArgumentException.class);
         medium.getMovie(name);
     }
 
     @Test
     public void getMovieNameEmptyTest() {
-        expected.expect(IllegalArgumentException.class);
+        
         String name = "";
         Medium medium = new Medium(new Category(1, 1, "new"));
+        expected.expect(IllegalArgumentException.class);
         medium.getMovie(name);
     }
 
@@ -84,123 +81,6 @@ public class MediumTest {
         assertEquals(id, medium.getId());
         assertEquals(movies, medium.getMovies());
         assertEquals(category, medium.getCategory());
-    }
-
-    /**
-     * Test of getId method, of class Medium.
-     */
-    @Test
-    public void testGetId() {
-
-        Medium instance;
-        instance = new Medium(new Category(5, 2, "new"));
-        instance.setId(5);
-        Integer expResult = 5;
-        Integer result = instance.getId();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setId method, of class Medium.
-     */
-    @Test
-    public void testSetId() {
-
-        Integer id = 4;
-        Medium instance;
-        instance = new Medium(new Category(5, 2, "new"));
-        instance.setId(id);
-        assertEquals(instance.getId(), id);
-    }
-
-    /**
-     * Test of getCategory method, of class Medium.
-     */
-    @Test
-    public void testGetCategory() {
-
-        Medium instance;
-        Category category = new Category(4, 2, "new");
-        instance = new Medium(new Category(2, 2, "new"));
-        Category expResult = category;
-        instance.setCategory(category);
-        Category result = instance.getCategory();
-        assertEquals(expResult, result);
-
-    }
-
-    /**
-     * Test of setCategory method, of class Medium.
-     */
-    @Test
-    public void testSetCategory() {
-
-        Category category = new Category(5, 2, "new");
-        Medium instance = new Medium(new Category(2, 2, "new"));
-        instance.setCategory(category);
-        assertEquals(instance.getCategory(), category);
-    }
-
-    /**
-     * Test of getMovies method, of class Medium.
-     */
-    @Test
-    public void testGetMovies() {
-
-        Medium instance;
-
-        List<Movie> movies = new ArrayList<>();
-        Movie movie1 = new Movie("new1");
-        Movie movie2 = new Movie("new2");
-        movies.add(movie1);
-        movies.add(movie2);
-        instance = new Medium(new Category(2, 2, "new"));
-        instance.addMovie(movie1);
-        instance.addMovie(movie2);
-        List<Movie> expResult = movies;
-        List<Movie> result = instance.getMovies();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of addMovie method, of class Medium.
-     */
-    @Test
-    public void testAddMovie() {
-
-        Movie movie = new Movie("new");
-        Movie movie2 = new Movie("new2");
-        List<Movie> movies = new ArrayList<>();
-        movies.add(movie);
-        movies.add(movie2);
-        Medium instance = new Medium(new Category(2, 2, "new"));
-        instance.addMovie(movie);
-        instance.addMovie(movie2);
-        assertEquals(instance.getMovies(), movies);
-
-    }
-
-    /**
-     * Test of getMovie method, of class Medium.
-     */
-    @Test
-    public void testGetMovie() {
-        String name = "new";
-        String name1 = "";
-        String name2 = null;
-        Movie movie = new Movie("new");
-        Movie movie2 = new Movie("new2");
-        List<Movie> movies = new ArrayList<>();
-        movies.add(movie);
-        movies.add(movie2);
-        Medium instance = new Medium(new Category(2, 2, "new"));
-        instance.addMovie(movie);
-        instance.addMovie(movie2);
-        Integer expResult = movies.indexOf(movie);
-        Integer result = instance.getMovie(name);
-        assertEquals(expResult, result);
-        assertFalse("Must not be empty", name.equals(name1));
-        assertFalse("Must not be null", name.equals(name2));
     }
 
 }
