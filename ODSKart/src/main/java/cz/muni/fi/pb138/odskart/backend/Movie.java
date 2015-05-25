7737b95b143e94b99b9e2f43c29b37a7554f607e
VojtaOndrej <Vojtech-Ondrej@seznam.cz>
2015-05-26 01:24:06 +0200

    Repeating code in Movie fixed.

--- ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Movie.java
+++ ODSKart/src/main/java/cz/muni/fi/pb138/odskart/backend/Movie.java
@@ -1,45 +1,21 @@
 package cz.muni.fi.pb138.odskart.backend;
 
-import java.util.Objects;
 
-public class Movie {
+public final class Movie {
 
     private String name;
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 97 * hash + Objects.hashCode(this.name);
-        return hash;
-    }
+    
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Movie other = (Movie) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
-    }
-
-    public Movie(String name) {
-        if (name == null || name.equals("")) {
-            throw new IllegalArgumentException("name is null or empty string");
-        }
-        this.name = name;
+    public Movie(String name) {        
+        setName(name);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public final void setName(String name) {
         if (name == null || name.equals("")) {
             throw new IllegalArgumentException("name is null or empty string");
         }
